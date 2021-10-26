7d50ab020e9635864c92b1b17f1a7366a1ea1ace
Jane Vo <jvo@educationfinanceinstitute.org>
2021-10-25 19:45:45 -0500

    re-wrote html, js and css code due to forgetting original work

---
+++ assets/javascript/questions.js
@@ -0,0 +1,45 @@
+//All quiz questions with choices and correct answer
+const questions = [
+  {
+    numb: 1,
+    question: 'Which is one of the 3 languages all web developers must learn?',
+    answer: 'Javascript',
+    options: [
+        'Python',
+        'React', 
+        'Javascript',
+        'SQL'
+    ]
+  },
+  {
+    numb: 2,
+    question: 'Which is not a keyword in Javascript to declare variables?',
+    answer: 'If',
+    options: [
+      'Var', 
+      'If', 
+      'Let', 
+      'Const'
+    ]
+  },
+  {
+    numb: 3,
+    question: 'An expression is a combination of values, variables, and operators, which computes to a value.',
+    answer: 'True',
+    options: [
+      'True', 
+      'False'
+    ]
+  },
+  {
+    numb: 4,
+    question: 'All JavaScript identifiers are _______.',
+    answer: 'case sensitive',
+    options: [
+      'capital letters', 
+      'case sensitive', 
+      'lowercase letters',
+      'characters', 
+    ]
+  },
+];
